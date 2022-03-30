da12b003232932dd1988fde15dd69e47739b8981
Raie01 <88224803+Raie01@users.noreply.github.com>
2022-03-30 09:39:26 +0800

    ayos na punyemas ka

--- new-edit-students.php
+++ new-edit-students.php
@@ -25,7 +25,7 @@ include('connection.php');
 <?php include 'includes/topnav.php';?>
 
 <div class="container-xl bg-white">
-    <form action="new-edit-students.php" id="register_form" class="pb-3 pt-2 mx-0" method="POST">
+    <form novalidate action="new-edit-students.php" id="up_form" class="pb-3 pt-2 mx-0" method="POST">
     <fieldset>
         <section class="form-top d-flex flex-row justify-content-around align-items-center">
           <img src="src/images/DepEd.png" width="120" height="120" alt="">
@@ -1333,12 +1333,7 @@ include('connection.php');
       </table>
       </div>
     </div>
-      <a class="btn btn-default next-form" style="float:right;" href="#register_form"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" 
-        fill="#4D96FF" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-        <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-        </svg>
-      </a>
-      <!-- <input type="button" class="next-form text-end btn btn-success" value="Next" /> -->
+      <input type="button" class="next-form text-end btn btn-success" style="float: right;" value="Next" />
     </fieldset>
     <fieldset>
       <!-- back -->
@@ -2250,19 +2245,14 @@ include('connection.php');
       </table>
       </div>
     </div>
-      <a class="btn btn-default previous-form" name="previous" href="#register_form"><svg xmlns="http://www.w3.org/2000/svg" width="35" 
-        height="35" fill="red" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
-          <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
-        </svg>
-      </a>
+      <input type="button" name="previous" style="float:left;" class="previous-form btn btn-default" value="Previous" /> 
       <input type="submit" name="update" style="float:right;" class="submitbtn btn btn-success" value="submit">
-
       <!-- <input type="button" class="next-form text-end btn btn-success" value="Next" /> -->
     </fieldset>
-      <!-- <input type="button" name="previous" style="float:left;" class="previous-form btn btn-default" value="" /> -->
+      <!-- -->
   </form>
 </div>
-<script src="src/js/stepper.js"></script>
+<!-- <script src="src/js/stepper.js"></script> -->
 <script src="src/js/number_limitation.js"></script>
 <?php
 include 'includes/footer.php';
--- src/js/number_limitation.js
+++ src/js/number_limitation.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+    var form_count = 1, previous_form, next_form, total_forms;
+    total_forms = $("fieldset").length;  
+    $(".next-form").click(function(){
+        previous_form = $(this).parent();
+        next_form = $(this).parent().next();
+        next_form.show();
+        previous_form.hide();
+    });  
+    $(".previous-form").click(function(){
+        previous_form = $(this).parent();
+        next_form = $(this).parent().prev();
+        next_form.show();
+        previous_form.hide();
+    });
+
     var invalidInputs = ["-","+","e",]; 
     $('#grade').keydown( function(e){
         if ($(this).val().length >= maxChar) { 
