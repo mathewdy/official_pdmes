e7488aeaa7c3d6ac44c8ffa8b66751925fe5d5b7
mathewdy <88813049+mathewdy@users.noreply.github.com>
2022-04-02 22:54:22 +0800

    Updated
    Eto yung sa Add records

---
+++ sample2.php
@@ -0,0 +1,63 @@
+
+<table>
+    <thead>
+        <tr>
+            <th>No.</th>
+            <th>Total Remakrs</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        include('connection.php');
+
+
+
+        $sql = "SELECT COUNT(remarks) AS 'total_remarks' , lrn FROM student_final_ratings WHERE phase = '1' AND lrn = '109857060083' AND remarks= 'FAILED' ";
+        $run = mysqli_query($conn,$sql);
+
+        if(mysqli_num_rows($run) > 0){
+            $number = 0;
+            foreach($run as $row){
+
+                
+
+                $number++;
+
+                if($row ['total_remarks'] >= 3){
+                    echo "retained"; 
+                }else if ($row ['total_remarks'] == 2){
+                    echo "Remedial";
+                }else if($row['total_remarks'] <= 1){
+                    echo "Promoted";
+                }
+                ?>
+
+                    <tr>
+                        <td><?php echo $number; ?></td>
+                        <td><?php echo $row ['total_remarks']?></td>
+
+                    
+
+                        <br>
+
+                        
+                    </tr>
+
+                    
+
+
+                <?php
+                
+
+                
+            }
+            
+        }
+
+
+        
+
+        ?>
+
+    </tbody>
+</table>
