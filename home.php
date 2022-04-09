c050580ed132d79c49087af84c59fec45389f5d8
Raie01 <88224803+Raie01@users.noreply.github.com>
2022-04-10 02:53:17 +0800

    Update home.php

--- home.php
+++ home.php
@@ -12,37 +12,19 @@ if(isset($_GET['page'])){
     $page = 1;
 }
 
-$num_per_age = 10;
+$num_per_page = 10;
 $start_from = ($page-1)*10;
-
-
-
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-<br>
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/topnav.php'; ?>
 
 
-
+<div class="container">
 <form action="" method="POST">
-
-    <br>
     <input type="text" name="search" placeholder="Search">
-
 </form>
-
-
-<table>
-
+<table class="table table-striped">
     <thead>
         <tr>
             <th>No.</th>
@@ -53,62 +35,56 @@ $start_from = ($page-1)*10;
     </thead>
     <tbody>
         <?php
+        $query = "SELECT * FROM `learners_personal_infos` ORDER BY last_name LIMIT $start_from, $num_per_page";
+        $run = mysqli_query($conn,$query);
 
-            $query = "SELECT * FROM `learners_personal_infos` ORDER BY last_name";
-            $run = mysqli_query($conn,$query);
-
-            if(mysqli_num_rows($run) > 0){
-                $number = 0;
-                foreach($run as $row ){
-                    $number++;
-                    ?>
-
-
-                        <tr>
-                            <td><?php echo $number;?></td>
-                            <td><?php echo $row ['first_name'] . $row ['last_name']?></td>
-                            <td><?php echo $row ['lrn']?></td>
-                            <td>
-                                <a href="">Edit</a>
-                                <a href="">Delete</a>
-                                <a href="">View</a>
-                            </td>
-                        </tr>
-
-
-                    <?php
-                }
-            }
-
+        if(mysqli_num_rows($run) > 0){
+            $number = 0;
+            foreach($run as $row ){
+                $number++;
+        ?>
+            <tr>
+                <td><?php echo $number;?></td>
+                <td><?php echo $row ['first_name'] . $row ['last_name']?></td>
+                <td><?php echo $row ['lrn']?></td>
+                <td>
+                    <a href="">Edit</a>
+                    <a href="">Delete</a>
+                    <a href="">View</a>
+                </td>
+            </tr>
+        <?php
+        }
+        }
         ?>
     </tbody>
-
 </table>
-    
+<?php
+    $pr_query = "SELECT * FROM learners_personal_infos";
+    $pr_results = mysqli_query($conn,$pr_query);
+    $total_record = mysqli_num_rows($pr_results);
+
+    $total_page = ceil($total_record/$num_per_page);
+
+
+    if($page > 1 ){
+        echo  "<a href='home.php?page=".($page-1)."' class='btn btn-danger'>Previous</a> ";
+    }
+
+    for($i=1;$i<$total_page;$i++){
+        echo  "<a href='home.php?page=".$i."' class='btn btn-primary'>$i</a> ";
+    }
+
+    if($i > $page ){
+        echo  "<a href='home.php?page=".($page+1)."' class='btn btn-danger'>Next</a> ";
+    }
+
+?>
+</div>
 </body>
 </html>
 
 <?php
-
-
-$pr_query = "SELECT * FROM learners_personal_infos";
-$pr_results = mysqli_query($conn,$pr_query);
-$total_record = mysqli_num_rows($pr_results);
-
-$total_page = ceil($total_record/$num_per_age);
-
-
-if($page > 1 ){
-    echo  "".($page-1)."' class='btn btn-danger'>Previous</a> ";
-}
-
-for($i=1;$i<$total_page;$i++){
-    echo  "".$i."' class='btn btn-primary'>$i</a> ";
-}
-
-if($i > $page ){
-    echo  "".($page+1)."' class='btn btn-danger'>Next</a> ";
-}
 
 ob_end_flush();
 
