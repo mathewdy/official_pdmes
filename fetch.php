cd9018b49d585183aa57c44894fe2cf9b54bd43f
Raie01 <88224803+Raie01@users.noreply.github.com>
2022-04-11 03:15:22 +0800

    home update
    Table with pagination & fucking live search

---
+++ fetch.php
@@ -0,0 +1,238 @@
+<style>
+    table{
+        border: 2px solid black;
+    }
+</style>
+<?php
+// include 'connection.php';
+$connect = new PDO("mysql:host=localhost; dbname=official_pdmes", "root", "");
+
+/*function get_total_row($connect)
+{
+  $query = "
+  SELECT * FROM tbl_webslesson_post
+  ";
+  $statement = $connect->prepare($query);
+  $statement->execute();
+  return $statement->rowCount();
+}
+
+$total_record = get_total_row($connect);*/
+
+$limit = '10';
+$page = 1;
+if($_POST['page'] > 1)
+{
+  $start = (($_POST['page'] - 1) * $limit);
+  $page = $_POST['page'];
+}
+else
+{
+  $start = 0;
+}
+
+$query = "
+SELECT * FROM learners_personal_infos
+";
+
+if($_POST['query'] != '')
+{
+  $query .= '
+  WHERE first_name LIKE "%'.str_replace(' ', '%', $_POST['query']).'%" OR
+  middle_name LIKE "%'.str_replace(' ', '%', $_POST['query']).'%" OR
+  last_name LIKE "%'.str_replace(' ', '%', $_POST['query']).'%" OR
+  lrn LIKE "%'.str_replace(' ', '%', $_POST['query']).'%"  
+  ';
+}
+
+$query .= 'ORDER BY id ASC ';
+
+$filter_query = $query . 'LIMIT '.$start.', '.$limit.'';
+$current_total = ($limit + $start);
+$statement = $connect->prepare($query);
+$statement->execute();
+$total_data = $statement->rowCount();
+
+$statement = $connect->prepare($filter_query);
+$statement->execute();
+$result = $statement->fetchAll();
+$total_filter_data = $statement->rowCount();
+
+$number=0;
+$output = '
+
+<table class="table table-hover table-light table-striped text-center">
+<thead>
+  <tr>
+    <th>No.</th>
+    <th>LRN</th>
+    <th>Student Name</th>
+    <th colspan="2">Actions</th>
+  </tr>
+</thead>
+';
+if($total_data > 0)
+{
+  foreach($result as $row)
+  {
+    $lrn = $row['lrn'];
+    $parse_lrn = intval($lrn);
+    $int_lrn = (($parse_lrn * 123456789 * 5977)/ 859475);
+    $edit_link = "new-edit-students.php?sid=". urlencode(base64_encode($int_lrn));
+    $view_link = "view-student-profile.php?sid=" . urlencode(base64_encode($int_lrn));
+    $delete_link = "delete-student.php?sid=" . urlencode(base64_encode($int_lrn));
+    $number++;
+    $output .= '
+    <tr class="clickable-row text-center" data-href="'.$view_link.'" style="cursor:pointer;">
+        <td>'.$number.'</td>
+        <td>'.$row["lrn"].'</td>
+        <td>'.$row["first_name"].' '.$row["middle_name"].' '.$row["last_name"].'</td>
+        <td class="d-flex flex-row justify-content-center">
+            <a href="'.$edit_link.'"><i style="color:#56BBF1; font-size:25px;" class="fa-solid fa-pen-to-square"></i></a>
+            <a href="'.$delete_link.'"><i style="color:red; font-size:25px;" class="fa-solid fa-circle-minus"></i></a>
+        </td>
+    </tr>
+    ';
+  }
+}
+else
+{
+  $output .= '
+  <tr>
+    <td colspan="2" align="center">No Data Found</td>
+  </tr>
+  ';
+}
+
+$output .= '
+</table>
+<br />
+<div>
+  <label style="float:left;">Showing '.$start.' to '.$current_total.'</label>
+  <ul style="float:right;" class="pagination">
+';
+
+$total_links = ceil($total_data/$limit);
+$previous_link = '';
+$next_link = '';
+$page_link = '';
+
+//echo $total_links;
+
+if($total_links > 4)
+{
+  if($page < 5)
+  {
+    for($count = 1; $count <= 5; $count++)
+    {
+      $page_array[] = $count;
+    }
+    $page_array[] = '...';
+    $page_array[] = $total_links;
+  }
+  else
+  {
+    $end_limit = $total_links - 5;
+    if($page > $end_limit)
+    {
+      $page_array[] = 1;
+      $page_array[] = '...';
+      for($count = $end_limit; $count <= $total_links; $count++)
+      {
+        $page_array[] = $count;
+      }
+    }
+    else
+    {
+      $page_array[] = 1;
+      $page_array[] = '...';
+      for($count = $page - 1; $count <= $page + 1; $count++)
+      {
+        $page_array[] = $count;
+      }
+      $page_array[] = '...';
+      $page_array[] = $total_links;
+    }
+  }
+}
+else
+{
+  for($count = 1; $count <= $total_links; $count++)
+  {
+    $page_array[] = $count;
+  }
+}
+
+for($count = 0; $count < count($page_array); $count++)
+{
+  if($page == $page_array[$count])
+  {
+    $page_link .= '
+    <li class="page-item active">
+      <a class="page-link" href="#">'.$page_array[$count].' <span class="sr-only">(current)</span></a>
+    </li>
+    ';
+
+    $previous_id = $page_array[$count] - 1;
+    if($previous_id > 0)
+    {
+      $previous_link = '<li class="page-item"><a class="page-link" href="javascript:void(0)" data-page_number="'.$previous_id.'">Previous</a></li>';
+    }
+    else
+    {
+      $previous_link = '
+      <li class="page-item disabled">
+        <a class="page-link" href="#">Previous</a>
+      </li>
+      ';
+    }
+    $next_id = $page_array[$count] + 1;
+    if($next_id >= $total_links)
+    {
+      $next_link = '
+      <li class="page-item disabled">
+        <a class="page-link" href="#">Next</a>
+      </li>
+        ';
+    }
+    else
+    {
+      $next_link = '<li class="page-item"><a class="page-link" href="javascript:void(0)" data-page_number="'.$next_id.'">Next</a></li>';
+    }
+  }
+  else
+  {
+    if($page_array[$count] == '...')
+    {
+      $page_link .= '
+      <li class="page-item disabled">
+          <a class="page-link" href="#">...</a>
+      </li>
+      ';
+    }
+    else
+    {
+      $page_link .= '
+      <li class="page-item"><a class="page-link" href="javascript:void(0)" data-page_number="'.$page_array[$count].'">'.$page_array[$count].'</a></li>
+      ';
+    }
+  }
+}
+
+$output .= $previous_link . $page_link . $next_link;
+$output .= '
+  </ul>
+
+</div>
+';
+
+echo $output;
+
+?>
+<script>
+    $(document).ready(function($) {
+        $(".clickable-row").click(function() {
+            window.location = $(this).data("href");
+        });
+    });
+</script>
--- home.php
+++ home.php
@@ -3,89 +3,52 @@ ob_start();
 include('connection.php');
 session_start();
 include('security.php');
-
-
-
-if(isset($_GET['page'])){
-    $page = $_GET['page'];
-}else{
-    $page = 1;
-}
-
-$num_per_page = 10;
-$start_from = ($page-1)*10;
 ?>
 
 <?php include 'includes/header.php'; ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/placeholder-loading/dist/css/placeholder-loading.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 <?php include 'includes/topnav.php'; ?>
-
-
-<div class="container">
-<form action="" method="POST">
-    <input type="text" name="search" placeholder="Search">
-</form>
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th>No.</th>
-            <th>Full Name</th>
-            <th>LRN</th>
-            <th>Options</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        $query = "SELECT * FROM `learners_personal_infos` ORDER BY last_name LIMIT $start_from, $num_per_page";
-        $run = mysqli_query($conn,$query);
-
-        if(mysqli_num_rows($run) > 0){
-            $number = 0;
-            foreach($run as $row ){
-                $number++;
-        ?>
-            <tr>
-                <td><?php echo $number;?></td>
-                <td><?php echo $row ['first_name'] . $row ['last_name']?></td>
-                <td><?php echo $row ['lrn']?></td>
-                <td>
-                    <a href="">Edit</a>
-                    <a href="">Delete</a>
-                    <a href="">View</a>
-                </td>
-            </tr>
-        <?php
-        }
-        }
-        ?>
-    </tbody>
-</table>
-<?php
-    $pr_query = "SELECT * FROM learners_personal_infos";
-    $pr_results = mysqli_query($conn,$pr_query);
-    $total_record = mysqli_num_rows($pr_results);
-
-    $total_page = ceil($total_record/$num_per_page);
-
-
-    if($page > 1 ){
-        echo  "<a href='home.php?page=".($page-1)."' class='btn btn-danger'>Previous</a> ";
-    }
-
-    for($i=1;$i<$total_page;$i++){
-        echo  "<a href='home.php?page=".$i."' class='btn btn-primary'>$i</a> ";
-    }
-
-    if($i > $page ){
-        echo  "<a href='home.php?page=".($page+1)."' class='btn btn-danger'>Next</a> ";
-    }
-
-?>
-</div>
-</body>
+    <br />
+    <div class="container">
+        <div class="form-group d-flex justify-content-between">
+            <a href="new-student.php" class="btn btn-success">Add New Record</a>
+            <input type="text" name="search_box" id="search_box" placeholder="Search..." />
+        </div>
+        <div id="dynamic_content"></div>
+    </div>
+  </body>
 </html>
+<script>
+  $(document).ready(function(){
 
-<?php
+    load_data(1);
 
-ob_end_flush();
+    function load_data(page, query = '')
+    {
+      $.ajax({
+        url:"fetch.php",
+        method:"POST",
+        data:{page:page, query:query},
+        success:function(data)
+        {
+          $('#dynamic_content').html(data);
+        }
+      });
+    }
 
-?>
+    $(document).on('click', '.page-link', function(){
+      var page = $(this).data('page_number');
+      var query = $('#search_box').val();
+      load_data(page, query);
+    });
+
+    $('#search_box').keyup(function(){
+      var query = $('#search_box').val();
+      load_data(1, query);
+    });
+
+  });
+</script>
